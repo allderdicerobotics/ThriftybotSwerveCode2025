4fcc32c7b02ab90f8b3c6cca1df37f36a94a14fa
Steel Dragons <taylorallderdicesteeldragons@gmail.com>
2025-08-05 17:40:28 -0400

    added swerve code

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,76 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.PS5Controller;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.generated.SwerveConstants;
+
+
+public class Robot extends TimedRobot {
+  // private final XboxController m_controller = new XboxController(0);
+  private final PS5Controller m_controller = new PS5Controller(0);
+  AHRS gyro = new AHRS(SPI.Port.kMXP); 
+
+  
+  
+
+  private final Drivetrain m_swerve = new Drivetrain(() -> Rotation2d.fromDegrees(gyro.getYaw()), new Pose2d());  // private final SimDrivetrain m_simSwerve = new SimDrivetrain(new Pose2d());
+
+  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
+  private final SlewRateLimiter m_xspeedLimiter = new SlewRateLimiter(20);
+  private final SlewRateLimiter m_yspeedLimiter = new SlewRateLimiter(20);
+  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(20);
+
+  @Override
+  public void robotPeriodic() {
+      // This runs in all robot modes (disabled, auto, teleop, test)
+      m_swerve.periodic();
+  } 
+
+  @Override
+  public void autonomousPeriodic() {
+    driveWithJoystick(false);
+    m_swerve.updateOdometry();
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    driveWithJoystick(true);
+  }
+
+  private void driveWithJoystick(boolean fieldRelative) {
+    // Get the x speed. We are inverting this because Xbox controllers return
+    // negative values when we push forward.
+    final var xSpeed =
+        -m_xspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftY(), 0.01))
+            * SwerveConstants.TOP_SPEED_METERS_PER_SEC;
+
+    // Get the y speed or sideways/strafe speed. We are inverting this because
+    // we want a positive value when we pull to the left. Xbox controllers
+    // return positive values when you pull to the right by default.
+    final var ySpeed =
+        -m_yspeedLimiter.calculate(MathUtil.applyDeadband(m_controller.getLeftX(), 0.01))
+            * SwerveConstants.TOP_SPEED_METERS_PER_SEC;
+
+    // Get the rate of angular rotation. We are inverting this because we want a
+    // positive value when we pull to the left (remember, CCW is positive in
+    // mathematics). Xbox controllers return positive values when you pull to
+    // the right by default.
+    final var rot =
+        -m_rotLimiter.calculate(MathUtil.applyDeadband(m_controller.getRightX(), 0.02))
+            ;
+
+    m_swerve.drive(xSpeed, ySpeed, rot, fieldRelative, getPeriod());
+    // m_simSwerve.drive(xSpeed, ySpeed, rot, fieldRelative, getPeriod());
+  }
+}
